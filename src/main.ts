6e8693d86c5c9c890d86c702c77f95ae91c99bd6
Stepan Suvorov <stevermeister@gmail.com>
2023-12-27 08:54:09 +0100

    cleaup: old cache management

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,14 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 
 
 // temporary solution to clean up cache from the old version 
-async function cleanAllCache() {
-    console.log('Started cleanup');
-    const cacheNames = await caches.keys();
-    for (const cacheName of cacheNames) {
-        if(cacheName.slice(0,27) === 'dutch-tax-income-calculator') {
-          await caches.delete(cacheName);
-          console.log(`Cache with key ${cacheName} deleted`)
-        }
-    }
-}
-cleanAllCache();
+// async function cleanAllCache() {
+//     console.log('Started cleanup');
+//     const cacheNames = await caches.keys();
+//     for (const cacheName of cacheNames) {
+//         if(cacheName.slice(0,27) === 'dutch-tax-income-calculator') {
+//           await caches.delete(cacheName);
+//           console.log(`Cache with key ${cacheName} deleted`)
+//         }
+//     }
+// }
+// cleanAllCache();
